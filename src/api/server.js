952045341173f513c0ec219a332e43fdc0e1df29
Boguita <heberdgomez@hotmail.com>
2023-02-08 22:26:15 -0300

    fix bug server

--- src/api/server.js
+++ src/api/server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-const PORT = process.env || 3000;
-app.listen(PORT, () => console.log("Server Running"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Server Running in port " + port));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
