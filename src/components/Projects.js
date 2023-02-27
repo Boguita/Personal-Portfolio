fa6d160fd2b0e762dc1d880bfe2d5c57f8ddf392
Boguita <heberdgomez@hotmail.com>
2023-02-27 15:38:01 -0300

    add finished project to section projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import evolveGym from "../assets/img/evolve-gym.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -17,10 +18,10 @@ export const Projects = () => {
       url: "https://github.com/nsdonato/front-invoices",
     },
     {
-      title: "Business Startup",
+      title: "Gym Project",
       description: "Design & Development",
-      imgUrl: projImg2,
-      url: "https://github.com/nsdonato/front-invoices",
+      imgUrl: evolveGym,
+      url: "https://evolve-gym.vercel.app/",
     },
     {
       title: "Business Startup",
