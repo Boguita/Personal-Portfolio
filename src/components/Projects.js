acc0a6e34aa471fa67ab269dd3a8157e6eed4cd5
Boguita <heberdgomez@hotmail.com>
2023-03-01 14:16:21 -0300

    add new project "tic-tac-toe" to project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import tictactoe from "../assets/img/Tic-tac-toe.png";
 import evolveGym from "../assets/img/evolve-gym.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
@@ -24,9 +25,10 @@ export const Projects = () => {
       url: "https://evolve-gym.vercel.app/",
     },
     {
-      title: "Business Startup",
+      title: "Funny Game",
       description: "Design & Development",
-      imgUrl: projImg3,
+      imgUrl: tictactoe,
+      url: "https://tic-tac-toe-zeta-lemon.vercel.app/",
     },
     {
       title: "Business Startup",
